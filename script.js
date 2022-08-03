658def86bf38a4a126d466ce43a795a5f0e61110
Mayur Mallav Saikia <89935072+MayurMallavSaikia@users.noreply.github.com>
2022-08-03 16:06:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let http=require('http')                                        // Node Server
 http.createServer(function(req,res){
-    res.write('<h1>Hello Node Server !!</h1><br><a href="https://progradexpress.herokuapp.com/express">Click to see Express Server Results</a>')
+    res.write('<h1>Hello Node Server !!</h1><br><a href="https://progradexpress.herokuapp.com">Click to see Express Server Results</a>')
     res.end()
      
 }).listen( process.env.PORT || 8000,()=>{console.log('Node Server running at the port 8000')})
